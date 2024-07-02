bd26b5abcc4d77960030c5e86cf695e2ba9f4da3
Gabriel Pizorusso Chierentin <gabrielchierentin@mec.gov.br>
2024-07-02 14:56:24 -0300

    Resolução Exercício
    Exercício 1 e 2

---
+++ Python/Jornada de Dados/Bootcamp Python para dados/aula_01.py
@@ -0,0 +1,16 @@
+# Exercício 01
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+nome = input("Digite seu nome: ")
+num_caracteres = len(nome)
+
+print("Seu nome tem", num_caracteres, "caracteres.")
+
+
+
+# Exercício 02
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+n1 = float(input("Digite um valor: "))
+n2 = float(input("Digite outro valor: "))
+soma = n1 + n2
+
+print("A soma dos valores é: ", soma)
