c3028b009aeabdeccc535c5e2b694381dec8faba
Gabriel Pizorusso Chierentin <gabrielchierentin@mec.gov.br>
2024-07-03 11:08:29 -0300

    Resolução Exercício
    Exercício 21 ao 25

--- Python/Jornada de Dados/Bootcamp Python para dados/aula_02.py
+++ Python/Jornada de Dados/Bootcamp Python para dados/aula_02.py
@@ -1,9 +1,9 @@
 # ### Exercícios
 
-exercicio = int(input("Informe o número do exercício (1-20): "))
+exercicio = int(input("Informe o número do exercício (1-25): "))
 print()
 
-if exercicio < 1 or exercicio > 20:
+if exercicio < 1 or exercicio > 25:
     print("Exercício inválido.")
 else:
 
@@ -18,7 +18,7 @@ else:
 
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-    if exercicio == 2:
+    elif exercicio == 2:
         n1 = float(input("Digite um número: "))
         resto = n1 % 5
 
@@ -26,7 +26,7 @@ else:
 
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-    if exercicio == 3:
+    elif exercicio == 3:
         n1 = float(input("Digite um número: "))
         n2 = float(input("Digite outro número: "))
         multiplicacao = n1 * n2
@@ -35,7 +35,7 @@ else:
 
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-    if exercicio == 4:
+    elif exercicio == 4:
         n1 = int(input("Digite um número: "))
         n2 = int(input("Digite outro número: "))
         divisao = n1 // n2
@@ -43,7 +43,7 @@ else:
         print(f"A divisão inteira dos dois números é: {divisao}")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
-    if exercicio == 5:
+    elif exercicio == 5:
         n1 = float(input("Digite um número: "))
         quadrado = n1 ** 2
 
@@ -52,7 +52,7 @@ else:
 
 # #### Números de Ponto Flutuante (`float`)
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
-    if exercicio == 6:
+    elif exercicio == 6:
         n1 = float(input("Digite um número decimal: "))
         n2 = float(input("Digite outro número decimal: "))
         soma = n1 + n2
@@ -61,7 +61,7 @@ else:
 
 
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
-    if exercicio == 7:
+    elif exercicio == 7:
         n1 = float(input("Digite um número decimal: "))
         n2 = float(input("Digite outro número decimal: "))
         media = (n1 + n2) / 2
@@ -70,7 +70,7 @@ else:
 
 
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
-    if exercicio == 8:
+    elif exercicio == 8:
         base = float(input("Digite a base: "))
         expoente = float(input("Digite o expoente: "))
         potencia = base ** expoente
@@ -79,7 +79,7 @@ else:
 
 
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
-    if exercicio == 9:
+    elif exercicio == 9:
         celsius = float(input("Digite a temperatura em Celsius: "))
         fahrenheit = (celsius * 9 / 5) + 32
 
@@ -87,7 +87,7 @@ else:
 
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-    if exercicio == 10:
+    elif exercicio == 10:
         import math
 
         raio = float(input("Digite o raio do círculo: "))
@@ -98,21 +98,21 @@ else:
 
 # #### Strings (`str`)
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-    if exercicio == 11:
+    elif exercicio == 11:
         txt = input("Digite um texto: ")
 
         print(f"O texto em maiúsculo é: {txt.upper()}")
 
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
-    if exercicio == 12:
+    elif exercicio == 12:
         nome_completo = input("Digite seu nome completo: ")
 
         print(f"Seu nome em minúsculo é: {nome_completo.lower()}")
 
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
-    if exercicio == 13:
+    elif exercicio == 13:
         frase = input("Digite uma frase: ")
 
         if " " not in frase:
@@ -122,7 +122,7 @@ else:
 
 
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
-    if exercicio == 14:
+    elif exercicio == 14:
         data = input("Insira uma data no formato dd/mm/aaaa: ")
         dia, mes, ano = data.split("/")
 
@@ -130,7 +130,7 @@ else:
 
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
-    if exercicio == 15:
+    elif exercicio == 15:
         txt1 = input("Digite um texto: ")
         txt2 = input("Digite outro texto: ")
 
@@ -139,7 +139,7 @@ else:
 
 # #### Booleanos (`bool`)
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
-    if exercicio == 16:
+    elif exercicio == 16:
         print("NOTA: preencha com qualquer valor para 'True' ou deixe vazio para 'False'")
         v1 = bool(input("Digite um valor booleano: "))
         v2 = bool(input("Digite outro valor booleano: "))
@@ -149,7 +149,7 @@ else:
 
 
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
-    if exercicio == 17:
+    elif exercicio == 17:
         print("NOTA: preencha com qualquer valor para 'True' ou deixe vazio para 'False'")
         v1 = bool(input("Digite um valor booleano: "))
         v2 = bool(input("Digite outro valor booleano: "))
@@ -159,7 +159,7 @@ else:
 
 
 # 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
-    if exercicio == 18:
+    elif exercicio == 18:
         print("NOTA: preencha com qualquer valor para 'True' ou deixe vazio para 'False'")
         v1 = bool(input("Digite um valor booleano: "))
         invertido = not v1
@@ -168,7 +168,7 @@ else:
 
 
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
-    if exercicio == 19:
+    elif exercicio == 19:
         n1 = float(input("Digite um número: "))
         n2 = float(input("Digite outro número: "))
         
@@ -177,9 +177,139 @@ else:
 
 
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
-    if exercicio == 20:
+    elif exercicio == 20:
         n1 = float(input("Digite um número: "))
         n2 = float(input("Digite outro número: "))
         
         if n1 != n2:
             print("Os números fornecidos são diferentes")
+
+
+### Exercício 21: Conversor de Temperatura
+# Escreva um programa que converta a temperatura de Celsius para Fahrenheit. O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando `try-except`, garantir que a entrada seja numérica, tratando qualquer `ValueError`. Imprima o resultado em Fahrenheit ou uma mensagem de erro se a entrada não for válida.
+    elif exercicio == 21:
+        try:
+            celsius = float(input("Digite a temperatura em Celsius: "))
+            fahrenheit = (celsius * 9 / 5) + 32
+        
+            print(f"A temperatura em Fahrenheit é: {fahrenheit}°F")
+
+        except ValueError:
+            print("ERRO: entrada inválida; por favor, insira um número!")
+
+
+### Exercício 22: Verificador de Palíndromo
+# Crie um programa que verifica se uma palavra ou frase é um palíndromo (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações). Utilize `try-except` para garantir que a entrada seja uma string. Dica: Utilize a função `isinstance()` para verificar o tipo da entrada.
+    elif exercicio == 22:
+        MSG_ERRO = "ERRO: entrada inválida; por favor, insira uma palavra ou frase!"
+
+        txt = input("Digite uma palavra ou frase: ")
+        
+        if txt is not None and txt != "":
+            try:
+                if isinstance(txt, str):
+                    if txt.lower() == txt.lower()[::-1]:
+                        print("A palavra ou frase é um palíndromo!")
+                    else:
+                        print("A palavra ou frase não é um palíndromo!")
+            except:
+                print(MSG_ERRO)
+        else:
+            print(MSG_ERRO)
+
+
+### Exercício 23: Calculadora Simples
+# Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. Use `try-except` para lidar com divisões por zero e entradas não numéricas. Utilize `if-elif-else` para realizar a operação matemática baseada no operador fornecido. Imprima o resultado ou uma mensagem de erro apropriada.
+    elif exercicio == 23:
+        try:
+            n1 = float(input("Digite o 1° número: "))
+            n2 = float(input("Digite o 2° número: "))
+
+            operacao = input("Digite a operação desejada (+, -, *, /): ")
+
+            if isinstance(operacao, str):
+                if operacao == "+" or operacao.lower() == "adição":
+                    resultado = n1 + n2
+                elif operacao == "-" or operacao.lower() == "subtração":
+                    resultado = n1 - n2
+                elif operacao == "*" or operacao.lower() == "multiplicação":
+                    resultado = n1 * n2
+                elif operacao == "/" or operacao.lower() == "divisão":
+                    try:
+                        resultado = n1 / n2
+                    except ZeroDivisionError:
+                        print("ERRO: não é possível dividir por zero!")
+                        exit()
+                
+                print(f'O resultado da operação "{operacao}" de {n1} e {n2} é: {resultado}')
+            else:
+                print("ERRO: operação inválida!")
+        except ValueError:
+            print("ERRO: entrada inválida; por favor, insira um número!")
+
+
+### Exercício 24: Classificador de Números
+# Escreva um programa que solicite ao usuário para digitar um número. Utilize `try-except` para assegurar que a entrada seja numérica e utilize `if-elif-else` para classificar o número como "positivo", "negativo" ou "zero". Adicionalmente, identifique se o número é "par" ou "ímpar".
+    elif exercicio == 24:
+        try:
+            n1 = float(input("Digite um número: "))
+            
+            if isinstance(n1, float):
+                positivo, negativo, zero, par, impar = False, False, False, False, False
+
+                if n1 > 0:
+                    positivo = True
+                elif n1 < 0:
+                    negativo = True
+                else:
+                    zero = True
+
+                # Transformando em inteiro para verificar a paridade
+                if int(n1) % 2 == 0:
+                    par = True
+                else:
+                    impar = True
+
+                # Criando uma lista (tupla) de nomes que serão exibidos e valores (True/ False)
+                lista = zip(['positivo', 'negativo', 'zero', f'o inteiro {int(n1)} é par', 'ímpar'], [positivo, negativo, zero, par, impar])
+                
+                # valores = []
+                # for nome, valor in lista:
+                #     if valor:
+                #         valores.append(nome)
+                valores = [nome for nome, valor in lista if valor]
+                
+                resultado = " e ".join(valores)
+
+                print(f"O número inserido {n1} é {resultado}!")
+
+        except ValueError:
+            print("ERRO: entrada inválida; por favor, insira um número!")
+
+
+### Exercício 25: Conversão de Tipo com Validação
+# Crie um script que solicite ao usuário uma lista de números separados por vírgula. O programa deve converter a string de entrada em uma lista de números inteiros. Utilize `try-except` para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um inteiro, imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
+    elif exercicio == 25:
+        numeros = input("Digite uma lista de números (separados por vírgula): ")
+        
+        # Remover espaços em branco e transformar em uma lista
+        lista_numeros = numeros.replace(" ", "").split(',')
+
+        try:
+            nova_lista = []
+
+            for i in lista_numeros:
+                nova_lista.append(int(float(i)))                
+                
+            print(f'A lista de números inteiros é: {nova_lista}')
+
+            # Ordena a lista de forma ascendente
+            nova_lista.sort()
+            print(f'Ordem ascendente: {nova_lista}')
+
+            # Remove elementos duplicados
+            nova_lista = list(dict.fromkeys(nova_lista))
+            print(f'Numeros distintos: {nova_lista}')
+
+        except:
+            print("ERRO: entrada inválida; por favor, insira um número!")
