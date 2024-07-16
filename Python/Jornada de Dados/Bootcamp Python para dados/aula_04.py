b1406153d480edd46dfde089e34165f5cde1c9b2
Gabriel Pizorusso Chierentin <gabrielchierentin@mec.gov.br>
2024-07-15 22:03:29 -0300

    Resolução Exercício
    Exercício 6 ao 15

--- Python/Jornada de Dados/Bootcamp Python para dados/aula_04.py
+++ Python/Jornada de Dados/Bootcamp Python para dados/aula_04.py
@@ -66,3 +66,113 @@ for fruta in l_fruta:
 print(f'Total da lista de compras: R$ {total:.2f}')
 
 
+#%%
+# 6. Eliminação de Duplicatas: Dada uma lista de emails, remover todos os duplicados.
+emails : list = ["user@example.com", "admin@example.com", "user@example.com", "manager@example.com"]
+print(emails)
+
+emails_unicos = list(set(emails))
+print(emails_unicos)
+
+
+#%%
+# 7. Filtragem de Dados: Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
+idades : list = [22, 15, 30, 17, 18]
+maior_igual_18 : list = [idade for idade in idades if idade >= 18]
+
+print(maior_igual_18)
+
+
+#%%
+# 8. Ordenação Personalizada: Dada uma lista de dicionários representando pessoas, ordená-las pelo nome.
+pessoas : list = [
+    {"nome": "Alice", "idade": 30}
+,   {"nome": "Bob", "idade": 25}
+,   {"nome": "Carol", "idade": 20}
+]
+pessoas.sort(key=lambda pessoa: pessoa["nome"])
+
+print(pessoas)
+
+
+#%%
+# 9. Agregação de Dados: Dado um conjunto de números, calcular a média.
+numeros : list = [10, 20, 30, 40, 50]
+media : float = sum(numeros) / len(numeros)
+
+print("Média: ", media)
+
+
+#%%
+# 10. Divisão de Dados em Grupos: Dada uma lista de valores, dividir em duas listas: uma para valores pares e outra para ímpares.
+valores : list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+pares : list = [valor for valor in valores if valor % 2 == 0]
+impares : list = [valor for valor in valores if valor % 2 != 0]
+
+print("Pares: ", pares)
+print("Ímpares: ", impares)
+
+
+#%%
+# 11. Atualização de Dados: Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
+produtos = [
+    {"id": 1, "nome": "Teclado", "preco": 100}
+,   {"id": 2, "nome": "Mouse", "preco": 80}
+,   {"id": 3, "nome": "Monitor", "preco": 300}
+,   {"id": 4, "nome": "Monitor", "preco": 299}
+,   {"id": 5, "nome": "Monitor", "preco": 300.0}
+]
+
+print(produtos)
+
+for produto in produtos:
+    if produto["nome"] == "Monitor":
+        produto["preco"] = 192
+
+print(produtos)
+
+
+#%%
+# 12. Fusão de Dicionários: Dados dois dicionários, fundi-los em um único dicionário.
+dic_1 : dict = {"a": 1, "b": 2, "c": 10}
+dic_2 : dict = {"c": 3, "d": 4, "a": 10}
+
+dicionario : dict = {**dic_1, **dic_2}
+
+print(dicionario)
+
+
+#%%
+# 13. Filtragem de Dados em Dicionário: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
+estoque : dict = {"Teclado": 10, "Mouse": 0, "Monitor": 3, "CPU": 0}
+
+estoque_positivo = {produto: quantidade for produto, quantidade in estoque.items() if quantidade > 0}
+
+print(estoque_positivo)
+
+
+#%%
+# 14. Extração de Chaves e Valores: Dado um dicionário, criar listas separadas para suas chaves e valores.
+dicionario : dict = {"a": 1, "b": 2, "c": 3}
+
+chave : list = list(dicionario.keys())
+valor : list = list(dicionario.values())
+
+print(chave)
+print(valor)
+
+
+#%%
+# 15. Contagem de Frequência de Itens: Dada uma string, contar a frequência de cada caractere usando um dicionário.
+texto : str = "engenharia de dados"
+frequencia : dict = {}
+
+for caractere in texto:
+    if caractere in frequencia:
+        frequencia[caractere] += 1
+    else:
+        frequencia[caractere] = 1
+
+print(frequencia)
+
